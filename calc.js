34d2cb9a7962743ea015e889ffcf65f771a12ed1
Gary Siu <Gary@untypeable.com>
2015-08-12 19:43:28 +0100

    Added advanced functions

--- calc.js
+++ calc.js
@@ -1,18 +1,27 @@
 var consent = true;
 while (consent == true) {
-  var num1 = parseFloat(prompt('Gimme your first number!'));
-  var op = prompt('Do you want (a)ddition, (s)ubtraction, (m)ultiplication or (d)ivision?');
+  var num1 = parseFloat(prompt('Gimme your base number!'));
+  var op = prompt('Do you want (a)ddition, (s)ubtraction, (m)ultiplication, (d)ivision, s(q)uare root or po(w)er?');
   op = op.toLowerCase();
-  var num2 = parseFloat(prompt('Gimme your second number!'));
-  var answer = "";
-  if(op === "a") {
-    answer = num1 + num2;
-  } else if(op === "s") {
-    answer = num1 - num2;
-  } else if(op === "m") {
-    answer = num1 * num2;
-  } else if(op === "d") {
-    answer = num1 / num2;
+  if(op === "q") {
+    answer = Math.sqrt(num1);
+  } else {
+    if(op === "w") {
+      var num2 = parseFloat(prompt('Gimme your exponent!'));
+      answer = Math.pow(num1, num2);
+    }else {
+      var num2 = parseFloat(prompt('Gimme your second number!'));
+      var answer = "";
+      if(op === "a") {
+        answer = num1 + num2;
+      } else if(op === "s") {
+        answer = num1 - num2;
+      } else if(op === "m") {
+        answer = num1 * num2;
+      } else if(op === "d") {
+        answer = num1 / num2;
+      }
+    }
   }
   alert('Your answer: ' + answer);
   consent = confirm('Start again?')
