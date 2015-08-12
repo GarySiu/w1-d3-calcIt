b82cc330f79f0a3dd75483efb5d0be12a33314d7
Gary Siu <Gary@untypeable.com>
2015-08-12 19:03:36 +0100

    The other 3 basic operations

--- calc.js
+++ calc.js
@@ -1,7 +1,19 @@
 var consent = true;
 while (consent == true) {
   var num1 = parseFloat(prompt('Gimme your first number!'));
+  var op = prompt('Do you want (a)ddition, (s)ubtraction, (m)ultiplication or (d)ivision?');
+  op = op.toLowerCase();
   var num2 = parseFloat(prompt('Gimme your second number!'));
-  alert('Your total: ' + (num1 + num2))
+  var answer = "";
+  if(op === "a") {
+    answer = num1 + num2;
+  } else if(op === "s") {
+    answer = num1 - num2;
+  } else if(op === "m") {
+    answer = num1 * num2;
+  } else if(op === "d") {
+    answer = num1 / num2;
+  }
+  alert('Your answer: ' + answer);
   consent = confirm('Start again?')
 }
